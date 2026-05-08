390a703d8116ab85244e55c647e6844141032958
Vladosik <vg6860299@gmail.com>
2026-05-08 22:18:27 +0300

    HomeWork2

--- Program.cs
+++ Program.cs
@@ -1,80 +1,97 @@
-﻿class Homework
+﻿using System;
+using System.Linq;
+
+class Program
 {
     static void Main()
     {
         ////Task 1
-        Console.Write("Enter number (1-100): ");
 
-        string input = Console.ReadLine();
+        //int[,] matrix = new int[5, 5];
+        //Random rnd = new Random();
 
-        if (!int.TryParse(input, out int num))
-        {
-            Console.WriteLine("Error, an integer is required");
-            return;
-        }
+        //Console.WriteLine("Massif:");
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    for (int j = 0; j < 5; j++)
+        //    {
+        //        matrix[i, j] = rnd.Next(-100, 101);
+        //        Console.Write($"{matrix[i, j],5}");
+        //    }
+        //    Console.WriteLine();
+        //}
 
-        if (num < 1 || num > 100)
-        {
-            Console.WriteLine("Error, 1 and 100!");
-            return;
-        }
+        //int minVal = matrix[0, 0], maxVal = matrix[0, 0];
+        //int minRow = 0, minCol = 0, maxRow = 0, maxCol = 0;
 
-        if (num % 3 == 0 && num % 5 == 0)
-            Console.WriteLine("Fizz Buzz");
-        else if (num % 3 == 0)
-            Console.WriteLine("Fizz");
-        else if (num % 5 == 0)
-            Console.WriteLine("Buzz");
-        else
-            Console.WriteLine(num);
+        //for (int i = 0; i < 5; i++)
+        //{
+        //    for (int j = 0; j < 5; j++)
+        //    {
+        //        if (matrix[i, j] < minVal)
+        //        {
+        //            minVal = matrix[i, j];
+        //            minRow = i; minCol = j;
+        //        }
+        //        if (matrix[i, j] > maxVal)
+        //        {
+        //            maxVal = matrix[i, j];
+        //            maxRow = i; maxCol = j;
+        //        }
+        //    }
+        //}
+
+        //int minIndex = minRow * 5 + minCol;
+        //int maxIndex = maxRow * 5 + maxCol;
+
+        //int left = Math.Min(minIndex, maxIndex);
+        //int right = Math.Max(minIndex, maxIndex);
+
+        //long sum = 0;
+        //for (int k = left + 1; k < right; k++)
+        //{
+        //    int row = k / 5;
+        //    int col = k % 5;
+        //    sum += matrix[row, col];
+        //}
+
+        //Console.WriteLine($"\nMini: {minVal} on position [{minRow},{minCol}]");
+        //Console.WriteLine($"Max: {maxVal} on position [{maxRow},{maxCol}]");
+
+        //if (right - left <= 1)
+        //    Console.WriteLine("\nThere are no elements between the min and max..");
+        //else
+        //    Console.WriteLine($"\nSum of elements between min and max: {sum}");
 
         ////Task 2
 
-        //Console.Write("Enter the number: ");
-        //double num = double.Parse(Console.ReadLine());
+        Console.Write("Enter the text: ");
+        string text = Console.ReadLine() ?? "";
 
-        //Console.Write("Enter a percentage of a number: ");
-        //double percent = double.Parse(Console.ReadLine());
+        Console.Write("Enter the offset (1-25): ");
+        int shift = int.Parse(Console.ReadLine() ?? "3") % 26;
 
-        //if (num < 1 || num > 100 || percent < 1 || percent > 100)
-        //{
-        //    Console.WriteLine("Error, invalid!!!!!!!!");
-        //    return; 
-        //}
+        string encrypted = CaesarCipher.Encrypt(text, shift);
+        string decrypted = CaesarCipher.Decrypt(encrypted, shift);
 
-        //double result = num * percent / 100;
-
-        //Console.WriteLine("Result -> " + result); // дуже не привичний синаксиз, я замість "+" ставив "," та думав чого не працює :) 
-
-
-        //Task 3 з цим завданням дуже довго грався, але зробив всі захисти від користувача
-
-        //Console.Write("Enter 4 numbers: ");
-        //string[] parts = Console.ReadLine().Split(' ');
-
-        //if (parts.Length != 4)
-        //{
-        //    Console.WriteLine("Error, 4 digits are required.");
-        //    return;
-        //}
-
-        //if (!int.TryParse(parts[0], out int a) ||
-        //    !int.TryParse(parts[1], out int b) ||
-        //    !int.TryParse(parts[2], out int c) ||
-        //    !int.TryParse(parts[3], out int d))
-        //{
-        //    Console.WriteLine("Error, an integer is required.");
-        //    return;
-        //}
-
-        //if (a < 0 || a > 9 || b < 0 || b > 9 || c < 0 || c > 9 || d < 0 || d > 9)
-        //{
-        //    Console.WriteLine("Error, 0 to 9!");
-        //    return;
-        //}
-
-        //int number = a * 1000 + b * 100 + c * 10 + d;
-
-        //Console.WriteLine("Result number: " + number);
+        Console.WriteLine($"\nOriginal:  {text}");
+        Console.WriteLine($"Encrypted: {encrypted}");
+        Console.WriteLine($"Decrypted: {decrypted}");
     }
+}
+
+class CaesarCipher
+{
+    static char CaesarChar(char c, int shift)
+    {
+        if (!char.IsLetter(c)) return c;
+        char baseChar = char.IsUpper(c) ? 'A' : 'a';
+        return (char)(baseChar + (c - baseChar + shift) % 26);
+    }
+
+    public static string Encrypt(string text, int shift) =>
+        new string(text.Select(c => CaesarChar(c, shift)).ToArray());
+
+    public static string Decrypt(string text, int shift) =>
+        Encrypt(text, 26 - shift);
 }
