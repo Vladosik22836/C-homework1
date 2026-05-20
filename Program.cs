02e57f4c7d933a13206965f7a7425cb9b9816cc7
Vladosik <vg6860299@gmail.com>
2026-05-20 12:47:14 +0300

    думаю подумати

--- Program.cs
+++ Program.cs
@@ -1,85 +1,83 @@
 ﻿using System;
 
-namespace DeviceApp
+namespace ArrayCalcExample
 {
-    // Base class
-    public abstract class Device
+    
+    interface ICalc
     {
-        public string Name { get; set; }
-        public string Characteristics { get; set; }
+        int Less(int valueToCompare);
+        int Greater(int valueToCompare);
+    }
 
-        protected Device(string name, string characteristics)
+    class Array : ICalc
+    {
+        private int[] numbers;
+
+        public Array(int[] numbers)
         {
-            Name = name;
-            Characteristics = characteristics;
+            this.numbers = numbers;
         }
 
-        public abstract void Sound();
+        public int Less(int valueToCompare)
+        {
+            int count = 0;
+
+            foreach (int num in numbers)
+            {
+                if (num < valueToCompare)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public int Greater(int valueToCompare)
+        {
+            int count = 0;
+
+            foreach (int num in numbers)
+            {
+                if (num > valueToCompare)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
 
         public void Show()
         {
-            Console.WriteLine($"Device Name: {Name}");
+            Console.WriteLine("Array elements:");
+
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine();
         }
-
-        public void Desc()
-        {
-            Console.WriteLine($"Description: {Characteristics}");
-        }
-    }
-
-    // Derived class: Kettle
-    public class Kettle : Device
-    {
-        public Kettle(string name, string characteristics) : base(name, characteristics) { }
-
-        public override void Sound() => Console.WriteLine("Sound: Ssss-Whistle!");
-    }
-
-    // Derived class: Microwave
-    public class Microwave : Device
-    {
-        public Microwave(string name, string characteristics) : base(name, characteristics) { }
-
-        public override void Sound() => Console.WriteLine("Sound: Hummm... Beeep!");
-    }
-
-    // Derived class: Car
-    public class Car : Device
-    {
-        public Car(string name, string characteristics) : base(name, characteristics) { }
-
-        public override void Sound() => Console.WriteLine("Sound: Vroom Vroom!");
-    }
-
-    // Derived class: Steamboat
-    public class Steamboat : Device
-    {
-        public Steamboat(string name, string characteristics) : base(name, characteristics) { }
-
-        public override void Sound() => Console.WriteLine("Sound: Tooooot-Tooooot!");
     }
 
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            // Creating instances
-            Device[] devices = new Device[]
-            {
-                new Kettle("Electric Kettle", "1.7L, 2200W, Stainless Steel"),
-                new Microwave("Samsung Smart", "800W, 23L, Ceramic Inside"),
-                new Car("Tesla Model 3", "Electric, Autopilot, Dual Motor"),
-                new Steamboat("Titanic II", "Steam engine, Luxury interior")
-            };
+            int[] data = { 5, 10, 15, 20, 25, 30 };
 
-            // Displaying information for each device
-            foreach (var device in devices)
-            {
-                device.Show();
-                device.Desc();
-                device.Sound();
-                Console.WriteLine(new string('-', 30));
-            }
+            Array array = new Array(data);
+
+            array.Show();
+
+            int value = 18;
+
+            Console.WriteLine($"\nComparison value: {value}");
+
+            Console.WriteLine($"Number of elements less than {value}: {array.Less(value)}");
+
+            Console.WriteLine( $"Number of elements greater than {value}: {array.Greater(value)}");
         }
     }
 }
