edd1d364f2bc32137fb134851ffcc591e1dee4d6
Vladosik <vg6860299@gmail.com>
2026-05-26 11:22:58 +0300

    Finished FootballTeam task

--- Program.cs
+++ Program.cs
@@ -1,156 +1,150 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 //Task 1
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Func<string, string> getRainbowColor = delegate (string color)
-        {
-            switch (color.ToLower())
-            {
-                case "red":
-                    return "RGB(255, 0, 0)";
-                case "orange":
-                    return "RGB(255, 165, 0)";
-                case "yellow":
-                    return "RGB(255, 255, 0)";
-                case "green":
-                    return "RGB(0, 255, 0)";
-                case "blue":
-                    return "RGB(0, 0, 255)";
-                case "indigo":
-                    return "RGB(75, 0, 130)";
-                case "violet":
-                    return "RGB(238, 130, 238)";
-                default:
-                    return "Unknown color";
-            }
-        };
-
-        Console.WriteLine(getRainbowColor("red"));
-        Console.WriteLine(getRainbowColor("green"));
-        Console.WriteLine(getRainbowColor("blue"));
-        Console.WriteLine(getRainbowColor("yellow"));
-        Console.WriteLine(getRainbowColor("black")); 
-    }
-}
-
-//Task 2
-//namespace BackpackApp
+// Базовий клас 
+//class SeaCreature
 //{
-//    class Item
-//    {
-//        public string Name { get; set; }
-//        public double Size { get; set; }
+//    public string Name { get; set; }
+//    public string Species { get; set; }
 
-//        public Item(string name, double size)
+//    public SeaCreature(string name, string species)
+//    {
+//        Name = name;
+//        Species = species;
+//    }
+
+//    public virtual void ShowInfo()
+//    {
+//        Console.WriteLine($"{Species}: {Name}");
+//    }
+//}
+
+//// Клас Риба
+//class Fish : SeaCreature
+//{
+//    public Fish(string name)
+//        : base(name, "Fish") { }
+//}
+
+//// Клас Акула
+//class Shark : SeaCreature
+//{
+//    public Shark(string name)
+//        : base(name, "Shark") { }
+//}
+
+//// Клас Дельфін
+//class Dolphin : SeaCreature
+//{
+//    public Dolphin(string name)
+//        : base(name, "Dolphin") { }
+//}
+
+//class Oceanarium : IEnumerable<SeaCreature>
+//{
+//    private List<SeaCreature> creatures = new List<SeaCreature>();
+
+//    public void AddCreature(SeaCreature creature)
+//    {
+//        creatures.Add(creature);
+//    }
+
+//    public IEnumerator<SeaCreature> GetEnumerator()
+//    {
+//        foreach (var creature in creatures)
 //        {
-//            Name = name;
-//            Size = size;
+//            yield return creature;
 //        }
 //    }
 
-//    class Backpack
+//    IEnumerator IEnumerable.GetEnumerator()
 //    {
-//        public string Color { get; set; }
-//        public string Brand { get; set; }
-//        public string Fabric { get; set; }
-//        public double Weight { get; set; }
-//        public double Capacity { get; set; }
-
-//        public List<Item> Content = new List<Item>();
-
-//        public event Action<Item> ItemAdded;
-//        public event Action<Item> ItemRemoved;
-//        public event Action Changed;
-
-//        public void AddItem(Item item)
-//        {
-//            double used = 0;
-
-//            foreach (var i in Content)
-//                used += i.Size;
-
-//            if (used + item.Size > Capacity)
-//                throw new Exception("Backpack is full!");
-
-//            Content.Add(item);
-//            ItemAdded?.Invoke(item);
-//        }
-
-//        public void RemoveItem(Item item)
-//        {
-//            Content.Remove(item);
-//            ItemRemoved?.Invoke(item);
-//        }
-
-//        public void ChangeSettings(string color, string brand, string fabric, double weight, double capacity)
-//        {
-//            Color = color;
-//            Brand = brand;
-//            Fabric = fabric;
-//            Weight = weight;
-
-//            double used = 0;
-//            foreach (var i in Content)
-//                used += i.Size;
-
-//            if (used > capacity)
-//                throw new Exception("New capacity is too small!");
-
-//            Capacity = capacity;
-//            Changed?.Invoke();
-//        }
+//        return GetEnumerator();
 //    }
+//}
 
-//    class Program
+//class Program
+//{
+//    static void Main()
 //    {
-//        static void Main(string[] args)
+//        Oceanarium oceanarium = new Oceanarium();
+
+//        oceanarium.AddCreature(new Fish("Nemo"));
+//        oceanarium.AddCreature(new Shark("Bruce"));
+//        oceanarium.AddCreature(new Dolphin("Flipper"));
+
+//        Console.WriteLine("Aquarium inhabitants: ");
+
+//        foreach (var creature in oceanarium)
 //        {
-//            Backpack backpack = new Backpack
-//            {
-//                Color = "Black",
-//                Brand = "Nike",
-//                Fabric = "Polyester",
-//                Weight = 1.2,
-//                Capacity = 10
-//            };
-
-//            backpack.ItemAdded += delegate (Item item)
-//            {
-//                Console.WriteLine("Added: " + item.Name);
-//            };
-
-//            backpack.ItemRemoved += delegate (Item item)
-//            {
-//                Console.WriteLine("Removed: " + item.Name);
-//            };
-
-//            backpack.Changed += delegate
-//            {
-//                Console.WriteLine("Backpack settings changed");
-//            };
-
-//            try
-//            {
-//                Item book = new Item("Book", 3);
-//                Item laptop = new Item("Laptop", 5);
-
-//                backpack.AddItem(book);
-//                backpack.AddItem(laptop);
-
-//                backpack.RemoveItem(book);
-
-//                backpack.ChangeSettings("Blue", "Adidas", "Leather", 1.5, 8);
-//            }
-//            catch (Exception ex)
-//            {
-//                Console.WriteLine("Error: " + ex.Message);
-//            }
+//            creature.ShowInfo();
 //        }
 //    }
 //}
 
+//Task 2
+
+// Клас Гравець
+class Player
+{
+    public string Name { get; set; }
+    public int Number { get; set; }
+    public string Position { get; set; }
+
+    public Player(string name, int number, string position)
+    {
+        Name = name;
+        Number = number;
+        Position = position;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"№{Number} {Name} - {Position}");
+    }
+}
+
+class FootballTeam : IEnumerable<Player>
+{
+    private List<Player> players = new List<Player>();
+
+    public void AddPlayer(Player player)
+    {
+        players.Add(player);
+    }
+
+    public IEnumerator<Player> GetEnumerator()
+    {
+        foreach (var player in players)
+        {
+            yield return player;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        FootballTeam team = new FootballTeam();
+
+        team.AddPlayer(new Player("Messi", 10, "Forward"));
+        team.AddPlayer(new Player("Ronaldo", 7, "Forward"));
+        team.AddPlayer(new Player("Neuer", 1, "Goalkeeper"));
+
+        Console.WriteLine("Football team composition:");
+
+        foreach (var player in team)
+        {
+            player.ShowInfo();
+        }
+    }
+}
